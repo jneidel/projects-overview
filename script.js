373cc90e738ad5d5ef1c952cde7498e38f3845bf
jneidel <jneidel@outlook.com>
2017-08-31 19:57:05 +0200

    Refactor/optimize code

--- script.js
+++ script.js
@@ -1,81 +1,48 @@
 "use strict";
 
-/* globals values */
+/* globals values names */
 
-const switchToHidden = { first  : false,
-    second : false,
-    third  : false,
-    forth  : false,
-    fifth  : false,
-    sixth  : false,
-    seventh: false,
-    eight  : false,
-    ninth  : false,
-    switchHidden( i ) {
-        let num;
-        switch ( i ) {
-            case 1 :
-                num = "first";
-                break;
-            case 2 :
-                num = "second";
-                break;
-            case 3 :
-                num = "third";
-                break;
-            case 4 :
-                num = "forth";
-                break;
-            case 5 :
-                num = "fifth";
-                break;
-            case 6 :
-                num = "sixth";
-                break;
-            case 7 :
-                num = "seventh";
-                break;
-            case 8 :
-                num = "eight";
-                break;
-            case 9 :
-                num = "ninth";
-        }
-        if ( switchToHidden[num] ) {
-            $( `#ItemHidden${i}` ).css( "display", "none" );
-            $( `#Item${i}` ).css( "display", "block" );
-            $( `#status${i}` ).html( " [a]" );
-            switchToHidden[num] = false;
-        } else {
-            $( `#ItemHidden${i}` ).css( "display", "block" );
-            $( `#Item${i}` ).css( "display", "none" );
-            $( `#status${i}` ).html( " [f]" );
-            switchToHidden[num] = true;
-        }
-    },
-};
+const hidden = [ null ];
+function switchToHidden( i ) {
+    if ( hidden[i] ) {
+        $( `#ItemHidden${i}` ).css( "display", "none" );
+        $( `#Item${i}` ).css( "display", "block" );
+        $( `#status${i}` ).html( " [a]" );
+        hidden[i] = false;
+    } else {
+        $( `#ItemHidden${i}` ).css( "display", "block" );
+        $( `#Item${i}` ).css( "display", "none" );
+        $( `#status${i}` ).html( " [f]" );
+        hidden[i] = true;
+    }
+}
 
 $( "document" ).ready( () => {
     for ( const container in values ) { // Get values form values.js
-        for ( const item in values[container] ) {
-            const i = values[container][item];
-            $( `#${container}` ).append( `<li>${i}</li>` );
-
-            if ( container.slice( 4, 10 ) === "Hidden" ) {
-                $( "#ItemHidden1" ).append( `<li>${i}</li>` );
-            } else {
-                $( "#Item1" ).append( `<li>${i}</li>` );
+        if ( container.slice( 0, 6 ) !== "hidden" ) {
+            $( "#container" ).append( `
+            <div id="Container${container}" class="item">
+                <h2>${names[container]}</h2><span id="status${container}" class="status"></span>
+                <p>
+                    <ul>
+                        <span id="Item${container}"></span>
+                        <span id="ItemHidden${container}" class="hidden"></span>
+                    </ul>
+                </p>
+            </div>` );
+            for ( const i in values[container] ) {
+                const item = values[container][i];
+                $( `#Item${container}` ).append( `<li>${item}</li>` );
+                $( "#Item1" ).append( `<li>${item}</li>` );
             }
+            for ( const i in values[`hidden${container}`] ) {
+                const item = values[`hidden${container}`][i];
+                $( `#ItemHidden${container}` ).append( `<li>${item}</li>` );
+                $( "#ItemHidden1" ).append( `<li>${item}</li>` );
+            }
+            hidden.push( false );
+            document.getElementById( `Container${container}` ).addEventListener( "dblclick", () => { switchToHidden( container ); } );
         }
     }
-    ( function setEventListeners( range ) {
-        for ( const i of range ) {
-            document.getElementById( `Container${i}` ).addEventListener( "dblclick", () => { switchToHidden.switchHidden( i ); } );
-        }
-    } )( range( 1, 9 ) );
-
-    function range( minNum, maxNum ) {
-        minNum -= 2;
-        return Array.from( new Array( maxNum - minNum - 1 ), ( x, i ) => i - minNum );
-    }
+    document.getElementById( `Container1` ).addEventListener( "dblclick", () => { switchToHidden( "1" ); } );
 } );
