678470977827c09699dd3ef0a0dda1acde780a6c
jneidel <jneidel@outlook.com>
2017-11-21 16:59:04 +0100

    Correct inputs, listeners

--- v2/controller/appController.js
+++ v2/controller/appController.js
@@ -1,10 +1,10 @@
 exports.renderList = async( req, res ) => {
     res.render( "main", { items: [ 
+        {title: "123aslkdjflkasdlfkj", values: [ "awe", "abc", "efd" ] }, 
+        {title: "asdfasdf", values: [ "awe", "abc", "efd" ] }, 
+        {title: "asdfasdfsadfasdfasdf", values: [ "awe", "abc", "efd" ] }, 
         {title: "yesadfsadfsadfsadfsadfasdah", values: [ "awe", "abc", "efd" ] }, 
-        {title: "yesadfsadfsadfsadfsadfasdah", values: [ "awe", "abc", "efd" ] }, 
-        {title: "yesadfsadfsadfsadfsadfasdah", values: [ "awe", "abc", "efd" ] }, 
-        {title: "yesadfsadfsadfsadfsadfasdah", values: [ "awe", "abc", "efd" ] }, 
-        {title: "yesadfsadfsadfsadfsadfasdah", values: [ "awe", "abc", "efd" ] }, 
-        {title: "yesadfsadfsadfsadfsadfasdah", values: [ "awe", "abc", "efd" ] } 
+        {title: "Okokokokok", values: [ "awe", "abc", "efd" ] }, 
+        {title: "hahahahahahahahahaha", values: [ "awe", "abc", "efd" ] } 
     ] } );
 };
--- v2/public/scripts/script.js
+++ v2/public/scripts/script.js
@@ -1,13 +1,32 @@
-const items = document.getElementsByClassName( "item" );
+const items = document.getElementsByClassName( "item" ),
+    titles = document.getElementsByClassName( "title" );
 
 for ( let item of items ) {
+    let originalItem = item.value;
     item.addEventListener( "keydown", async () => {
         if ( event.which === 13 ) {
             const parentNode = item.parentNode.parentNode.parentNode,
-                title = parentNode.childNodes[0].innerHTML;
+                title = parentNode.childNodes[0].value;
             
-            console.log( `Saving ${item.value} to database of ${title}.` );
+            console.log( `New item: ${item.value} --- Old item: ${originalItem} --- Title: ${title}` );
             // Send request to backend to change item database
+
+            originalItem = item.value;
+        }
+    } );
+}
+
+for ( let title of titles ) {
+    let originalTitle = title.value;
+    title.addEventListener( "keydown", async () => {
+        if ( event.which === 13 ) {
+
+            console.log( `New title: ${title.value} --- Old title: ${originalTitle} --- Title: ${originalTitle}`)
+            
+            // send ogTitle along on db request
+            // if len > 17 alert that it will probably be cut off
+
+            originalTitle = title.value;
         }
     } );
 }
