0e5430fc28037a094b640cc8471cad2a7c75feca
jneidel <jneidel@outlook.com>
2017-11-21 07:39:40 +0100

    Link styles to pug

--- v2/app.js
+++ v2/app.js
@@ -1,8 +1,13 @@
 const express = require( "express" ),
-    app = express();
+    app = express(),
+    bodyParser = require( "body-parser" );
 
 app.set( "view engine", "pug" );
 app.set( "views", `${__dirname}/views` );
+
+app.use( express.static( `${__dirname}/public` ) );
+
+app.use( bodyParser.json() );
 
 app.use( "/", require( "./routes/index" ) );
 
