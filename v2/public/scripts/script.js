3c8bd42844538f512ad5143b1a1e635a095e2731
jneidel <jneidel@outlook.com>
2017-11-21 20:24:16 +0100

    Add error handler, timeout to http request, refactor update request

--- v2/public/scripts/script.js
+++ v2/public/scripts/script.js
@@ -1,22 +1,30 @@
 const items = document.getElementsByClassName( "item" ),
     titles = document.getElementsByClassName( "title" );
 
+async function ajax( url ) {
+    const request = new XMLHttpRequest();
+    request.open( "POST", url, true );
+    request.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded; charset=UTF-8" );
+    request.addEventListener( "loadend", function requestLoad( event ) {
+        if ( this.status !== 200 ) {
+            alert( "There was a error saving your data." );
+        }
+    } );
+    request.timeout = 10000;
+    request.ontimeout = () => {
+        alert( "There was a timeout saving your data." );
+    };
+    await request.send();
+}
+
 for ( const item of items ) {
     let originalItem = item.value;
-    item.addEventListener( "keydown", async() => {
+    item.addEventListener( "keydown", () => {
         if ( event.which === 13 ) {
             const parentNode = item.parentNode.parentNode.parentNode,
                 title = parentNode.childNodes[0].value;
 
-            const request = new XMLHttpRequest();
-            request.open( "POST", `http://localhost:8080/api/update?newItem=${item.value}&oldItem=${originalItem}&title=${title}`, true );
-            request.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded; charset=UTF-8" );
-            request.addEventListener( "load", () => {
-                if ( request.status !== 200 ) {
-                    alert( "There was a error saving your data." );
-                }
-            } );
-            await request.send();
+            ajax( `http://localhost:8080/api/update?newItem=${item.value}&oldItem=${originalItem}&title=${title}` );
 
             originalItem = item.value;
         }
@@ -25,17 +33,9 @@ for ( const item of items ) {
 
 for ( const title of titles ) {
     let originalTitle = title.value;
-    title.addEventListener( "keydown", async() => {
+    title.addEventListener( "keydown", () => {
         if ( event.which === 13 ) {
-            const request = new XMLHttpRequest();
-            request.open( "POST", `http://localhost:8080/api/update?newTitle=${title.value}&title=${originalTitle}`, true );
-            request.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded; charset=UTF-8" );
-            request.addEventListener( "load", () => {
-                if ( request.status !== 200 ) {
-                    alert( "There was a error saving your data." );
-                }
-            } );
-            await request.send();
+            ajax( `http://localhost:8080/api/update?newTitle=${title.value}&title=${originalTitle}` );
 
             // if len > 17 alert that it will probably be cut off
 
