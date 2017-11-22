e5075f6d88f0ec9722135fc288f272cead24ed15
jneidel <jneidel@outlook.com>
2017-11-22 20:53:52 +0100

    Add dump helper

--- v2/app.js
+++ v2/app.js
@@ -1,6 +1,7 @@
 const express = require( "express" ),
     app = express(),
-    bodyParser = require( "body-parser" );
+    bodyParser = require( "body-parser" ),
+    helpers = require( "./helpers/helpers" );
 
 app.set( "view engine", "pug" );
 app.set( "views", `${__dirname}/views` );
@@ -9,6 +10,11 @@ app.use( express.static( `${__dirname}/public` ) );
 
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
+
+app.use( ( req, res, next ) => {
+    res.locals.h = helpers;
+    next();
+} );
 
 app.use( "/", require( "./routes/index" ) );
 
