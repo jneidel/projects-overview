4844168dc2dbff739d13d09c84d204c4b4c4be34
jneidel <jneidel@outlook.com>
2017-11-24 07:51:40 +0100

    Setup initial data rendering from database

--- v2/controller/appController.js
+++ v2/controller/appController.js
@@ -4,34 +4,35 @@ const mongo = require( "mongodb" ).MongoClient,
 require( "dotenv" ).config( { path: "../var.env" } );
 
 exports.renderItems = async( req, res ) => {
-    res.render( "main", {
-        cards: [
-            { title: "Programming", frontItems: [ "foo", "baz", "bar" ], backItems: [ "awe", "abc", "efd" ] },
-            { title: "Reading", frontItems: [ "foo", "baz", "bar" ], backItems: [ "awe", "abc", "efd" ] },
-            { title: "Courses", frontItems: [ "foo", "baz", "bar" ], backItems: [ "awe", "abc", "efd" ] },
-            { title: "eBooks", frontItems: [ "foo", "baz", "bar" ], backItems: [ "awe", "abc", "efd" ] },
-            { title: "Audiobooks", frontItems: [ "foo", "baz", "bar" ], backItems: [ "awe", "abc", "efd" ] },
-        ],
-        title: "Project Manager" } );
-};
-
-exports.updateDatabase = async( req, res ) => {
-    console.log( req.query );
+    const userid = 1;
 
     mongo.connect( process.env.DATABASE, ( err, db ) => {
         assert.equal( err, null );
 
-        const query = {},
-            projection = {},
-            cursor = db.collection( "test" ).find( query, projection );
+        const cards = [],
+            query = { userid },
+            projection = { _id: 0, title: 1, front: 1, back: 1, position: 1 },
+            cursor = db.collection( "cards" ).find( query, projection );
+
+        cursor.sort( { position: 1 } );
 
         cursor.forEach( ( doc ) => {
-            console.log( doc );
+            cards.push( doc );
         }, ( err ) => {
             assert.equal( err, null );
+
+            res.render( "main", {
+                cards,
+                title: "Project Manager",
+            } );
+
             return db.close();
         } );
     } );
+};
+
+exports.updateDatabase = async( req, res ) => {
+    console.log( req.query );
 
     res.sendStatus( 200 );
 };
