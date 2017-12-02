b1ae0c4cd282496e2f8f193bfffcafb543d13c65
jneidel <jneidel@outlook.com>
2017-12-02 18:41:17 +0100

    Add cards test data

---
+++ v2/data/cards-test.js
@@ -0,0 +1,42 @@
+db.cards.insertMany([
+{
+    _id: 1,
+    userid: "1",
+    title: "Programming", 
+    front: [ "foo", "baz", "bar", "" ], 
+    back: [ "awe", "abc", "efd", "" ],
+    position: 1,
+},
+{
+    _id: 2,
+    userid: "1",
+    title: "Reading", 
+    front: [ "foo", "baz", "bar", "" ], 
+    back: [ "awe", "abc", "efd", "" ],
+    position: 2,
+},
+{
+    _id: 3,
+    userid: "1",
+    title: "Courses", 
+    front: [ "foo", "baz", "bar", "" ], 
+    back: [ "awe", "abc", "efd", "" ],
+    position: 3,
+},
+{
+    _id: 4,
+    userid: "1",
+    title: "eBooks", 
+    front: [ "foo", "baz", "bar", "" ], 
+    back: [ "awe", "abc", "efd", "" ],
+    position: 4,
+},
+{
+	_id: 5,
+	userid: "1",
+	title: "Audiobooks", 
+    front: [ "foo", "baz", "bar", "" ], 
+    back: [ "awe", "abc", "efd", "" ],
+    position: 5,
+}
+])
