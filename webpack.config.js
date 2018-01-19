38ec84239016cdce4142036012545d4f252718d7
jneidel <jneidel@outlook.com>
2018-01-19 18:54:20 +0100

    Add scss processor, restructure webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,46 @@
 /* eslint-disable no-tabs */
 
+const webpack = require( "webpack" );
 const path = require( "path" );
 const minify = require( "babel-minify-webpack-plugin" );
-const webpack = require( "webpack" );
 
 require( "dotenv" ).config( { path: "variables.env" } );
 const prod = process.env.PROD || false;
 
 module.exports = {
+  entry: {
+    accountHandler      : "./src/scripts/accountHandler.js",
+    appClickHandler     : "./src/scripts/appClickHandler.js",
+    axios               : "./src/scripts/globalVariables/axios.js",
+    encryptWithPubKey   : "./src/scripts/globalVariables/encryptWithPubKey.js",
+    globals             : "./src/scripts/globalVariables/globals.js",
+    loginRegisterHandler: "./src/scripts/loginRegisterHandler.js",
+  },
+  output: {
+    path: path.resolve( __dirname, "public/scripts/" ),
+    filename: "[name].js"
+  },
+  module: {
+    loaders: prod ? [
+      {
+        test: /\.js$/,
+        use : [ {
+          loader: "babel-loader",
+          options: { presets: [ "babel-preset-env" ] },
+        } ],
+      },
+    ] : [],
+  },
+  plugins: prod ? [
+    new minify( {}, { comments: false } ),
+    new webpack.optimize.UglifyJsPlugin( {
+      compress: { warnings: false }
+    } ),
+  ] : [],
+}
+
+
+const old = {
   entry: {
     accountHandler      : "./src/accountHandler.js",
     appClickHandler     : "./src/appClickHandler.js",
@@ -26,7 +59,7 @@ module.exports = {
       test   : /\.js/,
       options: {
         loaders: [ "babel-loader" ],
-        presets: [ "@babel/preset-env" ],
+        presets: [ "babel-preset-env" ],
       },
     } ),
   ] : [],
