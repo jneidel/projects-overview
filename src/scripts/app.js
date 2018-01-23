adee848da6a6001c9b774b4a6fa5d6405719355d
jneidel <jneidel@outlook.com>
2018-01-23 15:58:15 +0100

    Restrict card flip
    Don't flip when clicking in an input field

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -95,13 +95,17 @@ function setEventListeners() {
   function flipCard( card ) {
     let cardState = "front";
 
-    card.addEventListener( "dblclick", () => {
-      if ( cardState === "front" ) {
-        card.style.transform = "rotateY( 180deg )";
-        cardState = "back";
-      } else {
-        card.style.transform = "rotateY( 0deg )";
-        cardState = "front";
+    card.addEventListener( "dblclick", ( event ) => {
+      const el = event.target.tagName;
+      const permitted = [ "SPAN", "DIV", "P" ];
+      if ( ~permitted.indexOf( el ) ) {
+        if ( cardState === "front" ) {
+          card.style.transform = "rotateY( 180deg )";
+          cardState = "back";
+        } else {
+          card.style.transform = "rotateY( 0deg )";
+          cardState = "front";
+        }
       }
     } );
   }
